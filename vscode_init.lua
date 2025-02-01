5ee8c4563aaf51cc880b3ea5f112b1c8d7efd047
Oleg Berezin <olegberezin2002@gmail.com>
2025-02-01 22:02:40 +0300

    add config

---
+++ vscode_init.lua
@@ -0,0 +1,45 @@
+-- <leader> key
+vim.g.mapleader = ' '
+
+-- relative number
+vim.wo.relativenumber = true
+
+-- open config
+vim.cmd('nmap <leader>c :e ~/.config/nvim/vscode_init.lua<cr>')
+
+-- save
+vim.cmd('nmap <leader>s :w<cr>')
+
+-- paste without overwriting
+vim.keymap.set('v', 'p', 'P')
+
+-- clear search highlighting
+vim.keymap.set('n', '<Esc>', ':nohlsearch<cr>')
+
+-- skip folds (down, up)
+vim.cmd('nmap j gj')
+vim.cmd('nmap k gk')
+
+-- sync system clipboard
+vim.opt.clipboard = 'unnamedplus'
+
+-- search ignoring case
+vim.opt.ignorecase = true
+
+-- disable "ignorecase" option if the search pattern contains upper case characters
+vim.opt.smartcase = true
+
+-- using black hole register for delete
+vim.keymap.set("n", "d", '"_d', { noremap = true })
+vim.keymap.set("n", "D", '"_D', { noremap = true })
+vim.keymap.set("v", "d", '"_d', { noremap = true })
+vim.keymap.set("v", "D", '"_D', { noremap = true })
+
+vim.keymap.set("n", "x", '"_x', { noremap = true })
+vim.keymap.set("v", "x", '"_x', { noremap = true })
+
+vim.keymap.set("n", "c", '"_c', { noremap = true })
+vim.keymap.set("n", "C", '"_C', { noremap = true })
+vim.keymap.set("v", "c", '"_c', { noremap = true })
+vim.keymap.set("v", "C", '"_C', { noremap = true })
+
