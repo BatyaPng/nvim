d7060c30511dcea8f0f5dd28396b3099b6f5abe5
Oleg Berezin <olegberezin2002@gmail.com>
2025-02-05 12:48:17 +0300

    add new feutures

--- vscode_init.lua
+++ vscode_init.lua
@@ -1,9 +1,6 @@
 -- <leader> key
 vim.g.mapleader = ' '
 
--- relative number
-vim.wo.relativenumber = true
-
 -- open config
 vim.cmd('nmap <leader>c :e ~/.config/nvim/vscode_init.lua<cr>')
 
@@ -13,11 +10,14 @@ vim.cmd('nmap <leader>s :w<cr>')
 -- close
 vim.cmd('nmap <leader>w :q<cr>')
 
+-- use blackhole register for action
+vim.cmd('nmap <leader>f "_')
+
 -- get file name
 vim.cmd('nmap <leader>n :let @+ = expand(\'%:t\')<CR>')
 
 -- go to definition
-vim.cmd("nmap <leader>d gd")
+vim.keymap.set('n', '<leader>d', 'gd')
 
 -- paste without overwriting
 vim.keymap.set('v', 'p', 'P')
@@ -37,18 +37,3 @@ vim.opt.ignorecase = true
 
 -- disable "ignorecase" option if the search pattern contains upper case characters
 vim.opt.smartcase = true
-
--- using black hole register for delete
-vim.keymap.set("n", "d", '"_d', { noremap = true })
-vim.keymap.set("n", "D", '"_D', { noremap = true })
-vim.keymap.set("v", "d", '"_d', { noremap = true })
-vim.keymap.set("v", "D", '"_D', { noremap = true })
-
-vim.keymap.set("n", "x", '"_x', { noremap = true })
-vim.keymap.set("v", "x", '"_x', { noremap = true })
-
-vim.keymap.set("n", "c", '"_c', { noremap = true })
-vim.keymap.set("n", "C", '"_C', { noremap = true })
-vim.keymap.set("v", "c", '"_c', { noremap = true })
-vim.keymap.set("v", "C", '"_C', { noremap = true })
-
